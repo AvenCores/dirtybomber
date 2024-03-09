476d4d75d31508fd092d3742c2a343f94d7661b8
avencores <64781822+AvenCores@users.noreply.github.com>
2024-03-09 12:56:53 +0300

    Update start.py

--- start.py
+++ start.py
@@ -8,8 +8,6 @@ class Start:
     def __init__(self):
         just_fix_windows_console()
         print(Fore.BLUE + Style.BRIGHT + "·▄▄▄▄  ▪  ▄▄▄  ▄▄▄▄▄ ▄· ▄▌    ▄▄▄▄·       • ▌ ▄ ·. ▄▄▄▄· ▄▄▄ .▄▄▄  \n██▪ ██ ██ ▀▄ █·•██  ▐█▪██▌    ▐█ ▀█▪▪     ·██ ▐███▪▐█ ▀█▪▀▄.▀·▀▄ █·\n" + Fore.YELLOW + Style.BRIGHT + "▐█· ▐█▌▐█·▐▀▀▄  ▐█.▪▐█▌▐█▪    ▐█▀▀█▄ ▄█▀▄ ▐█ ▌▐▌▐█·▐█▀▀█▄▐▀▀▪▄▐▀▀▄ \n██. ██ ▐█▌▐█•█▌ ▐█▌· ▐█▀·.    ██▄▪▐█▐█▌.▐▌██ ██▌▐█▌██▄▪▐█▐█▄▄▌▐█•█▌\n▀▀▀▀▀• ▀▀▀.▀  ▀ ▀▀▀   ▀ •     ·▀▀▀▀  ▀█▄▀▪▀▀  █▪▀▀▀·▀▀▀▀  ▀▀▀ .▀  ▀" + Style.RESET_ALL)
-        time.sleep(5)
-        webbrowser.open("https://t.me/dirtybomber")
         os.system('cls' if os.name == 'nt' else 'clear')
         Menu().mainMenu()
 
